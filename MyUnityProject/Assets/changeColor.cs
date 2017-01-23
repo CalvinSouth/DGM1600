312519252dbaf8381f9123119c8303aed2912cf2
Calvin South <southcalvinm@gmail.com>
2017-01-23 10:55:36 -0700

    Changed "changeColor.cs" to allow color to be Green or Blue

--- MyUnityProject/Assets/changeColor.cs
+++ MyUnityProject/Assets/changeColor.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 
 public class changeColor : MonoBehaviour {
 	void Update() {
-		if(Input.GetKeyDown(KeyCode.R))
-			GetComponent<Renderer>().material.color = Color.red;
-	}
+        
+
+        if (Input.GetKeyDown(KeyCode.R))
+            GetComponent<Renderer>().material.color = Color.red;
+        else if (Input.GetKeyDown(KeyCode.G))
+            GetComponent<Renderer>().material.color = Color.green;
+        else if (Input.GetKeyDown(KeyCode.B))
+            GetComponent<Renderer>().material.color = Color.blue;
+    }
 }
