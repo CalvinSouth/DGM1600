80f68abbcf3fb43a5c0813da0b18304950d8195a
Calvin South <southcalvinm@gmail.com>
2017-01-25 10:44:54 -0700

    Changed "changeColor.cs" to allow white, black, and yellow colors

--- MyUnityProject/Assets/changeColor.cs
+++ MyUnityProject/Assets/changeColor.cs
@@ -1,14 +1,39 @@
 using UnityEngine;
 
 public class changeColor : MonoBehaviour {
-	void Update() {
-        
+
+    // Update is called once per frame
+    void Update() {
 
         if (Input.GetKeyDown(KeyCode.R))
+        {
             GetComponent<Renderer>().material.color = Color.red;
+            print("Changed color to red");
+        }
         else if (Input.GetKeyDown(KeyCode.G))
+        {
             GetComponent<Renderer>().material.color = Color.green;
+            print("Changed color to green");
+        }
         else if (Input.GetKeyDown(KeyCode.B))
+        {
             GetComponent<Renderer>().material.color = Color.blue;
+            print("Changed color to blue");
+        }
+        else if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+            print("Changed color to white");
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Renderer>().material.color = Color.black;
+            print("Changed color to black");
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+            print("Changed color to yellow");
+        }
     }
 }
