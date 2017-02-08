2d75f5b7c8d091b8fc91544a94a56d34d457be96
Calvin South <southcalvinm@gmail.com>
2017-02-08 10:52:55 -0700

    Added "elseCause.cs" to "MyUnityProject"

---
+++ MyUnityProject/Assets/ElseClause.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElseClause : MonoBehaviour
+{
+
+    public string password;
+    public bool userAgreement;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (password == "OU812" && userAgreement)
+        {
+            print("Correct");
+        }
+        else
+        { print("Incorrect"); }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
