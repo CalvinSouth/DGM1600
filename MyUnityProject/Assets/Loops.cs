56f179a92839a5025fe485c2d1695ee00a463ed2
Calvin South <southcalvinm@gmail.com>
2017-02-13 10:56:15 -0700

    Added "Loops.cs" to "MyUnityProject"

---
+++ MyUnityProject/Assets/Loops.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loops : MonoBehaviour {
+
+    public string[] animals;
+
+    public string animal = "Wolf";
+    // Use this for initialization
+	void Start () {
+        foreach(string animalList in animals)
+        {
+            if(animal == animalList)
+            {
+                print("I have a match, it is " + animalList);
+            }
+            else
+            {
+                print(animalList + " is not a match");
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
