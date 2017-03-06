82e03d70c8138e99cf2f70738c08205ad5c49953
Calvin South <southcalvinm@gmail.com>
2017-03-06 10:52:51 -0700

    Added "Scope.cs" to "MyUnityProject"

---
+++ MyUnityProject/Assets/Scope.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scope : MonoBehaviour {
+
+    public string owner = "Bob";
+    private string renter = "Kate";
+
+    private void Awake()
+    {
+        string cat = "Joe";
+        if(cat == "Joe")
+        {
+            string mouse = "Helen";
+            print(cat + " ate " + mouse);
+        }
+        print(renter);
+        print(cat);
+        print(owner);
+    }
+
+	void Start () {
+        string cat = "Frank";
+        print(cat);
+        print(owner);
+	}
+}
