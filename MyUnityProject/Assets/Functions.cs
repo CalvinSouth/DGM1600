5b763154b6422f5cbf13cbd9abf0e3dfe985c463
Calvin South <southcalvinm@gmail.com>
2017-03-13 10:54:32 -0600

    Added "Functions.cs" to "MyUnityProject"

---
+++ MyUnityProject/Assets/Functions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Functions : MonoBehaviour {
+
+    public string userName;
+    public InputField myInputField;
+
+    public void UpdateUserName()
+    {
+        WelcomeUser(myInputField.text);
+    }
+
+    void WelcomeUser (string _name)
+    {
+        userName = _name;
+        print("Welcome " + _name + ", get ready to play!");
+    }
+
+}
