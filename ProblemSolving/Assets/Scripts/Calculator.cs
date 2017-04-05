5db51f03b17360067101336bd6c9f92189a4a3b2
Calvin South <southcalvinm@gmail.com>
2017-04-05 10:50:47 -0600

    Added public variables to 'Calculator.cs"

--- ProblemSolving/Assets/Scripts/Calculator.cs
+++ ProblemSolving/Assets/Scripts/Calculator.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Calculator : MonoBehaviour {
 
-
+    public float value1;
+    public float value2;
+    public float output;
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void readInput(GameObject val)
+    {
+        
+    }
 }
