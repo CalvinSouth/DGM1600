e8c3c210687c02976b399bdfb89f8724ace2f387
Calvin South <southcalvinm@gmail.com>
2017-04-07 01:35:34 -0600

    Completed "Calculator.cs"

--- ProblemSolving/Assets/Scripts/Calculator.cs
+++ ProblemSolving/Assets/Scripts/Calculator.cs
@@ -1,20 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class Calculator : MonoBehaviour {
-
-    public float value1;
-    public float value2;
+public class Calculator : MonoBehaviour
+{
+    public InputField val1, val2;
+    public InputField outputVal;
     public float output;
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-    public void readInput(GameObject val)
+    public void Calculate(string operate)
     {
-        
+        switch (operate) {
+            case "plus":
+                output = float.Parse(val1.text) + float.Parse(val2.text);
+                break;
+            case "minus":
+                output = float.Parse(val1.text) - float.Parse(val2.text);
+                break;
+            case "multiply":
+                output = float.Parse(val1.text) * float.Parse(val2.text);
+                break;
+            case "divide":
+                output = float.Parse(val1.text) / float.Parse(val2.text);
+                break;
+        }
+
+        outputVal.text = output.ToString();
     }
 }
